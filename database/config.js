f138b429818c6173e97abafcb8088ae681915e8a
David <david.torta.olah@gmail.com>
2024-07-02 20:21:47 +0200

    deprecated options removed

--- database/config.js
+++ database/config.js
@@ -4,10 +4,7 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
+        await mongoose.connect(process.env.DB_CNN);
 
             console.log('DB Online')
         
